01963e91c3f43e4beeb7f688c08c808d0796cfea
MacroUniverse <270174408@qq.com>
2022-07-31 20:34:53 -0500

    merge

--- IDE_test/main.cpp
+++ IDE_test/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <unordered_map>
@@ -9,28 +10,30 @@
 
 using namespace std;
 
-template<class T>
+template<class T> // from boost library
 inline void hash_combine(size_t &seed, const T &v) {
     seed ^= hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
-struct hash2 {
+struct hash_pair { // similar to std::hash, for pair<>
     template<class T, class T1>
     size_t operator()(const pair<T,T1> &a) const {
-        size_t h = hash<T>{}(a.first);
+        size_t h = 0;
+        hash_combine(h, a.first);
         hash_combine(h, a.second);
         return h;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    unordered_map<pair<string, int>, int, hash2> umap;
-    umap[pair<string,int>("abc", 123)] = 1;
-    umap[pair<string,int>("bcd", 123)] = 100;
-    umap[pair<string,int>("cde", 234)] = 200;
-    for (auto p:umap) {
-        cout << p.first.first << ", " << p.first.second << " -> " << p.second << endl;
+    vector<int> v;
+    int imax = numeric_limits<int>::max();
+    while (1) {
+        int tmp = imax;
+        cin >> tmp;
+        if (tmp == imax) break;
+        v.push_back(tmp);
+        cin.ignore(imax, '\n');
     }
     return 0;
 }
