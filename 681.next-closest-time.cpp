7fcf38c88a32a102702db83bbab075e1e1efe560
MacroUniverse <270174408@qq.com>
2022-09-27 12:11:52 -0500

    Update 681.next-closest-time.cpp

--- 681.next-closest-time.cpp
+++ 681.next-closest-time.cpp
@@ -4,7 +4,6 @@ public:
         char i[4];
         i[0] = time[0] - '0'; i[1] = time[1] - '0';
         i[2] = time[3] - '0'; i[3] = time[4] - '0';
-        string ret(time);
         int t0 = mins(i[0], i[1], i[2], i[3]);
         int min_dist = INT_MAX;
         for (auto i1 : i) {
@@ -20,14 +19,14 @@ public:
                         if (dist == 0) continue;
                         if (dist < min_dist) {
                             min_dist = dist;
-                            ret[0] = i1+'0'; ret[1] = i2+'0';
-                            ret[3] = i3+'0'; ret[4] = i4+'0';
+                            time[0] = i1+'0'; time[1] = i2+'0';
+                            time[3] = i3+'0'; time[4] = i4+'0';
                         }
                     }
                 }
             }
         }
-        return ret;
+        return time;
     }
 
     int mins(int i1, int i2, int i3, int i4) {
