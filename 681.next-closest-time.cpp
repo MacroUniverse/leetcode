2a4d3983083d4acca64fb9f3e470cbbc9049a9b1
MacroUniverse <270174408@qq.com>
2022-09-27 12:04:44 -0500

    Create 681.next-closest-time.cpp

---
+++ 681.next-closest-time.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string nextClosestTime(string time) {
+        char i[4];
+        i[0] = time[0] - '0'; i[1] = time[1] - '0';
+        i[2] = time[3] - '0'; i[3] = time[4] - '0';
+        string ret(time);
+        int t0 = mins(i[0], i[1], i[2], i[3]);
+        int min_dist = INT_MAX;
+        for (auto i1 : i) {
+            for (auto i2 : i) {
+                if (i1*10+i2 > 23)
+                    continue;
+                for (auto i3 : i) {
+                    for (auto i4 : i) {
+                        if (i3*10+i4 > 59)
+                            continue;
+                        int dist = mins(i1, i2, i3, i4) - t0;
+                        if (dist < 0) dist += 24*60;
+                        if (dist == 0) continue;
+                        if (dist < min_dist) {
+                            min_dist = dist;
+                            ret[0] = i1+'0'; ret[1] = i2+'0';
+                            ret[3] = i3+'0'; ret[4] = i4+'0';
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    int mins(int i1, int i2, int i3, int i4) {
+        return (i1*10+i2)*60 + i3*10+i4;
+    }
+};
